46c665bb7be8dadc19be1233ba982b4efd88ba57
kanika gulabani <kanika.gulabani@konsult.atg.se>
2019-10-06 20:41:19 +0200

    committed

--- src/main/java/com/hellokoding/auth/validator/UserValidator.java
+++ src/main/java/com/hellokoding/auth/validator/UserValidator.java
@@ -29,14 +29,5 @@ public class UserValidator implements Validator {
         if (userService.findByUsername(user.getUsername()) != null) {
             errors.rejectValue("username", "Duplicate.userForm.username");
         }
-
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
-        if (user.getPassword().length() < 8 || user.getPassword().length() > 32) {
-            errors.rejectValue("password", "Size.userForm.password");
-        }
-
-        if (!user.getPasswordConfirm().equals(user.getPassword())) {
-            errors.rejectValue("passwordConfirm", "Diff.userForm.passwordConfirm");
-        }
     }
 }
